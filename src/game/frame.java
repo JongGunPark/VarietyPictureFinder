8dd087ff4b7d9d12c5596ffd56274f71661e7399
ParkJongGun <33774031+JongGunPark@users.noreply.github.com>
2017-12-17 00:41:35 +0900

    Add files via upload
    전체적으로 수정한 프레임 간의 연결과 main문에서의 timer()가 실행되는 등의 경미한 오류를 제거하였다.

--- src/game/frame.java
+++ src/game/frame.java
@@ -1,109 +1,125 @@
 package game;
 
 import java.awt.Graphics;
+import javax.swing.Timer;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class frame extends JFrame {
-   private static final long serialVersionUID = 1L;
-   JPanel panel_main;
-   JPanel panel_stage1;
-   JPanel panel_howto;
-   JPanel panel_pause;
-   
-   public frame() {
-      setTitle("다양한 그림 찾기");
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      ImageIcon img = new ImageIcon("배경.jpg","배경");
-      panel_main = new panel_main(this);
+	private static final long serialVersionUID = 1L;
+	JPanel panel_main;
+	JPanel panel_fail;
+	JPanel panel_stagep1;
+	JPanel panel_stage1;
+	JPanel panel_stage2;
+	JPanel panel_howto;
+	
+	
+	public frame() {
+		setTitle("다양한 그림 찾기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ImageIcon img = new ImageIcon("배경.jpg","배경");
+		panel_main = new panel_main(this);
 
-      setSize(img.getIconWidth(),img.getIconHeight());
-      setLayout(null);
-   
-      add(panel_main);
-      panel_main.setVisible(true);
-         
-      panel_stage1 = new panel_stage1(this);
-      add(panel_stage1);
-      panel_stage1.setVisible(false);
-      ((game.panel_stage1) panel_stage1).timerStop();
-      
-      panel_howto = new panel_howto(this);
-      add(panel_howto);
-      panel_howto.setVisible(false);
-      
-      panel_pause = new panel_pause(this);
-      add(panel_pause);
-      panel_pause.setVisible(false);
-      
-      
-      setVisible(true);
-      setResizable(false); // 창 크기 고정                  
-   }
-//   public void stage1() {
-//      panel_main.setVisible(false);
-//      panel_stage1.setVisible(true);
-//   }
-//   public void howto() {
-//      panel_main.setVisible(false);
-//      add(panel_howto);
-//      panel_howto.setVisible(true);
-//   }
-//   
-   
-   public void change(String panel_name) {
-      if(panel_name.equals("panel_howto")) {
-         panel_main.setVisible(false);
-      
-         remove(panel_howto);
-         
-         panel_howto = new panel_howto(this);
-         add(panel_howto);
-         panel_howto.setVisible(true);
-         
-      }
-      else if(panel_name.equals("panel_stage1")) {
-         panel_main.setVisible(false);
-         panel_howto.setVisible(false);
-         
-         remove(panel_stage1);
-         
-         panel_stage1 = new panel_stage1(this);
-         add(panel_stage1);
-         panel_stage1.setVisible(true);
-         
-      }
-      else if(panel_name.equals("panel_pause")){
-    	  panel_stage1.setVisible(false);
-    	  
-    	  panel_pause = new panel_pause(this);
-    	  add(panel_pause);
-    	  panel_pause.setVisible(true);
-      }
-      
-   }
-   
-   public void change2(String Panel_name) {
-	   if(Panel_name.equals("panel_stage1")){
-		   panel_pause.setVisible(false);
+		setSize(img.getIconWidth(),img.getIconHeight());
+		setLayout(null);
+	
+		add(panel_main);
+		panel_main.setVisible(true);
+		
+//		panel_fail = new panel_stagep1(this);
+//		add(panel_fail);
+//		panel_fail.setVisible(false);
+		
+		panel_stagep1 = new panel_stagep1(this);
+		add(panel_stagep1);		
+		panel_stagep1.setVisible(false);
+		((game.panel_stagep1) panel_stagep1).timerStop();
+		
+		panel_stage1 = new panel_stage1(this);
+		add(panel_stage1);		
+		panel_stage1.setVisible(false);
+		((game.panel_stage1) panel_stage1).timerStop();
+		
 
-	       panel_stage1.setVisible(true);
-	       ((game.panel_stage1) panel_stage1).timerStart();
-
-	   }
-	   
-	   else if(Panel_name.equals("panel_main")){
-		   panel_pause.setVisible(false);
-		   remove(panel_pause);
-		   remove(panel_stage1);
-		   
-		   panel_main = new panel_main(this);
-		   add(panel_main);
-		   panel_main.setVisible(true);
-	   }
-   }
-   
-   public static void main(String[] args) {
-      new frame();
-   }
+		
+		panel_stage2 = new panel_stage2(this);
+		add(panel_stage2);
+		panel_stage2.setVisible(false);
+		panel_howto = new panel_howto(this);
+		add(panel_howto);
+		panel_howto.setVisible(false);
+		
+		
+		setVisible(true);
+		setResizable(false); // 창 크기 고정						
+	}
+//	public void stage1() {
+//		panel_main.setVisible(false);
+//		panel_stage1.setVisible(true);
+//	}
+//	public void howto() {
+//		panel_main.setVisible(false);
+//		add(panel_howto);
+//		panel_howto.setVisible(true);
+//	}
+//	
+	
+	public void change(String panel_name) {
+		if(panel_name.equals("panel_howto")) {
+			panel_main.setVisible(false);
+		
+			remove(panel_howto);
+			panel_howto = new panel_howto(this);
+			add(panel_howto);
+			panel_howto.setVisible(true);
+			
+		}
+		else if(panel_name.equals("panel_stagep1")) {
+			panel_main.setVisible(false);
+			
+			remove(panel_stagep1);
+			panel_stagep1 = new panel_stagep1(this);
+			add(panel_stagep1);
+			panel_stagep1.setVisible(true);
+			
+		}
+		else if(panel_name.equals("panel_fail")) {
+			panel_stage1.setVisible(false);
+			remove(panel_stage1);
+			panel_stage1 = new panel_stage1(this);
+			add(panel_main);
+			panel_main.setVisible(true);			
+		}
+		else if(panel_name.equals("panel_stage1")) {
+			panel_stagep1.setVisible(false);
+			
+			remove(panel_stage1);
+			panel_stage1 = new panel_stage1(this);
+			add(panel_stage1);
+			panel_stage1.setVisible(true);
+			
+		}
+//		else if(panel_name.equals("panel_stage1")) {
+//			panel_main.setVisible(false);
+//			
+//			remove(panel_stage1);
+//			panel_stage1 = new panel_stage1(this);
+//			add(panel_stage1);
+//			panel_stage1.setVisible(true);
+//			
+//		}
+		else if(panel_name.equals("panel_stage2")) {
+			panel_stage1.setVisible(false);
+			remove(panel_stage2);
+			panel_stage2 = new panel_stage2(this);
+			add(panel_stage2);
+			panel_stage2.setVisible(true);
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		new frame();
+	}
 }
--- src/game/panel_main.java
+++ src/game/panel_main.java
@@ -9,6 +9,7 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
+
 public class panel_main extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	private frame fr;
@@ -18,36 +19,22 @@ public class panel_main extends JPanel implements ActionListener {
 	
 	public panel_main(frame fr) {
 		this.fr=fr;
+		
 		ImageIcon img = new ImageIcon("배경.jpg","배경");
-		ImageIcon img2 = new ImageIcon("start.png","시작");
-		ImageIcon img22 = new ImageIcon("start2.png");
-		
-		ImageIcon img3= new ImageIcon("howto.png","방법");
-		ImageIcon img33= new ImageIcon("howto2.png");
-		
+		ImageIcon img2 = new ImageIcon("start.jpg","시작");
+		ImageIcon img3= new ImageIcon("howto.jpg","방법");
 		setBounds(0, 0, 1000, 600);
 		JLabel bg = new JLabel(img);
 		btn1 = new JButton("시작");
 		btn1.setIcon(img2);
-		btn1.addActionListener(this);
-		btn1.setBorderPainted(false);
-		btn1.setContentAreaFilled(false);
-		btn1.setFocusPainted(false);
-		btn1.setRolloverIcon(img22);
-		
-		
 		btn2 = new JButton("방법");
 		btn2.setIcon(img3);
+		btn1.addActionListener(this);
 		btn2.addActionListener(this);
-		btn2.setBorderPainted(false);
-		btn2.setContentAreaFilled(false);
-		btn2.setFocusPainted(false);
-		btn2.setRolloverIcon(img33);
-		
 		setLayout(null);		
-		btn1.setBounds(650,350,197, 80);
+		btn1.setBounds(650,350,200, 80);
 		add(btn1);
-		btn2.setBounds(650,450,197, 80);
+		btn2.setBounds(650,450,200, 80);
 		add(btn2);
 		add(bg);
 		bg.setBounds(0,0,img.getIconWidth(), img.getIconHeight()); // 1000x600
@@ -58,8 +45,8 @@ public class panel_main extends JPanel implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand() == "시작") 
-			fr.change("panel_stage1");
-		
+			fr.change("panel_stagep1");
+	
 		else if (e.getActionCommand() == "방법") {
 			fr.change("panel_howto");
 			
@@ -73,3 +60,4 @@ public class panel_main extends JPanel implements ActionListener {
 
 	
 }
+
