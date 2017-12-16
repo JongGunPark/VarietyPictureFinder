0f9bc6a167cb0781c4a535e1e1849e6d97ec3ff7
ParkSeHui <33440254+ParkSeHui@users.noreply.github.com>
2017-12-17 00:32:50 +0900

    Add files via upload

--- src/game/panel_stage1.java
+++ src/game/panel_stage1.java
@@ -16,23 +16,29 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.SwingConstants;
 import javax.swing.Timer;
 
 
 public class panel_stage1 extends JPanel implements ActionListener  {
-	
+
    JProgressBar p;
    Timer timer;
- 
-   static int[] imageX = {269, 7, 470, 136};
-   static int[] imageY = {102, 191, 106, 53};
+   Timer endtimer;
+   int lifeNum=3;
+   int questionNum=4;
+   int val;
+   frame fr;
+   static int[] imageX = {277, 7, 483, 139};
+   static int[] imageY = {101, 190, 110, 53};
    // 틀린부분의 범위 기본 설정값
    static final int range = 15;
    
-   static boolean f1 = false;
-   static boolean f2 = false;
-   static boolean f3 = false;
-   static boolean f4 = false;
+    boolean f1 = false;
+    boolean f2 = false;
+    boolean f3 = false;
+    boolean f4 = false;
+
    
    ImageIcon img = new ImageIcon("image1.jpg","Stage1_image1");
    JLabel bg = new JLabel(img);
@@ -43,14 +49,27 @@ public class panel_stage1 extends JPanel implements ActionListener  {
    ImageIcon circle = new ImageIcon("circle.png");
    Image cir = circle.getImage();
    
-//	int lifeNum=3;
-//	ImageIcon lifeIcon;
-//	ImageIcon life3 = new ImageIcon("Life3.jpg","3Life");
-//	ImageIcon life2 = new ImageIcon("Life2.jpg","2life");
-//	ImageIcon life1 = new ImageIcon("Life1.jpg","1life");
-	
+   ImageIcon clear2 = new ImageIcon("clear.png");
+   Image cle = clear2.getImage();
+   
+   ImageIcon life1 = new ImageIcon("life1.png","1life");
+   ImageIcon life2 = new ImageIcon("life2.png","2life");
+   ImageIcon life3 = new ImageIcon("life3.png","3Life");
+   JLabel life = new JLabel();   // 목숨에 대한 라벨 : 클릭 수에 따라 변화
+
+   ImageIcon que0 = new ImageIcon("zero.jpg");
+   ImageIcon que1 = new ImageIcon("one.jpg");
+   ImageIcon que2 = new ImageIcon("two.jpg");
+   ImageIcon que3 = new ImageIcon("three.jpg");
+   ImageIcon que4 = new ImageIcon("four.jpg");
+   JLabel question = new JLabel();   // 남은 문제의 갯수 라벨 : 정답 체크 수에 따라 변화
+   
+   
+   //JLabel clear = new JLabel();
    public panel_stage1(frame fr) {
-	   setBackground(new Color(252,241,192));
+      
+     this.fr=fr;
+     setBackground(new Color(252,241,192));
       CustomMouseAdapter cma = new CustomMouseAdapter (); 
       //image1 이미지가 저장된 JPanel a 객체에 감지기 설정
       bg.addMouseListener(cma);
@@ -59,24 +78,35 @@ public class panel_stage1 extends JPanel implements ActionListener  {
       
       setBounds(0, 0, 1000, 600);
       setLayout(null);
+//      JLabel title = new JLabel("STAGE1 틀린그림찾기");
+//      title.setForeground(Color.BLACK);
+//      title.setHorizontalAlignment(SwingConstants.LEFT);
+//      title.setBounds(0, 30, 1000, 10);
       JProgressBar progressBar = new JProgressBar();
-       JButton button = new JButton("Pause");
+      JButton button = new JButton("Pause");
       progressBar.setBackground(Color.white);
-       progressBar.setForeground(Color.YELLOW);
-       //progressBar.setBorder(BorderFactory.createLineBorder(Color.green));
-       progressBar.setBorderPainted(false);
-       progressBar.setBounds(0, 470, 1000, 20);
-       button.setBounds(870, 500, 100, 30);
-//	    LifeControl();
-//	    lifeDraw.setBounds(100, 500, 100, 50);
-//	    add(lifeDraw);
-       add(progressBar);
-       add(button);
+      progressBar.setForeground(new Color(184,119,84));
+      //progressBar.setBorder(BorderFactory.createLineBorder(Color.green));
+      progressBar.setBorderPainted(false);
+      progressBar.setBounds(0, 464, 1000, 20);
+      button.setBounds(870, 500, 100, 30);
+      life.setBounds(0, 480, 200, 100);
+      life.setIcon(new ImageIcon("life3.png","3life"));  
+      question.setBounds(200, 480, 200, 100);
+      question.setIcon(new ImageIcon("four.jpg","4que"));
+      //clear.setBounds(150,150,600,300);
+      //clear.setIcon(new ImageIcon("clear.jpg"));
+//     add(title);
+      add(progressBar);
+      add(life);
+      add(question);
+      
+      add(button);
        progressBar.setValue(100);
 
        ActionListener updateProBar = new ActionListener() {
          public void actionPerformed(ActionEvent actionEvent) {
-           int val = progressBar.getValue();
+           val = progressBar.getValue();
            progressBar.setValue(--val);
            System.out.println(progressBar.getValue());
          }
@@ -85,28 +115,31 @@ public class panel_stage1 extends JPanel implements ActionListener  {
        timer = new Timer(300, updateProBar);
        timer.start();
        timer.isRunning();
-
        
-
        button.addActionListener(new ActionListener() {
-         public void actionPerformed(ActionEvent e) {
-           if (timer.isRunning()) {
-             timer.stop();
-             button.setText("Start");
-           } else if (button.getText() != "Pause") {
-             timer.start();
-             button.setText("Pause");
+           public void actionPerformed(ActionEvent e) {
+             if (timer.isRunning()) {
+               timer.stop();         
+               fr.change("panel_pause");
+             } 
+             
            }
-         }
-       });
-      //timer();
-      bg.setBounds(10,55,img.getIconWidth(), img.getIconHeight());
+         });
+
+
+      
+      //add(clear);
+      //clear.setVisible(false);
+      bg.setBounds(0,65,img.getIconWidth(), img.getIconHeight());
       add(bg);
-      bg2.setBounds(502,55,img2.getIconWidth(), img2.getIconHeight());
+      bg2.setBounds(500,65,img2.getIconWidth(), img2.getIconHeight());
       add(bg2);
       
-      setVisible(true);
       
+      setVisible(true);
+
+      
+
    }
 
    
@@ -125,31 +158,84 @@ public class panel_stage1 extends JPanel implements ActionListener  {
       public void mouseReleased(MouseEvent e) {
        // 마우스클릭한 곳의 x,y좌표값을 얻어와 Point 객체를 생성
        Point p = e.getPoint();
+
        if (isRangeof(0,p)) {
+         if (f1 == false) {
+         questionNum--;
+         }
           f1 = true; 
           bg.repaint();
           bg2.repaint(); //양쪽모두 동그라미 표시를 위한 repaint
-
+          
        } else if (isRangeof(1,p)) {
+          if (f2 == false) {
+             questionNum--;
+             }
           f2 = true;
           bg.repaint();
           bg2.repaint();
-
+          
        } else if (isRangeof(2,p)) {
+          if (f3 == false) {
+             questionNum--;
+             }
           f3 = true;
           bg.repaint();
           bg2.repaint();
-
+          
        } else if (isRangeof(3,p)) {
+          if (f4 == false) {
+             questionNum--;
+           }
           f4 = true;
           bg.repaint();
           bg2.repaint();
-
-       } 
-//       
+          
+       } else {
+             lifeNum--;
+          System.out.println(lifeNum);
+           
+          if (lifeNum==2) {
+             life.setIcon(life2);
+          }
+          else if(lifeNum==1) {
+             life.setIcon(life1);   
+          }
+                       
+          
+       }
+      if (questionNum==4) {
+         question.setIcon(que4);
+      }
+      else if(questionNum==3) {
+         question.setIcon(que3);
+      }else if(questionNum==2) {
+         question.setIcon(que2);
+      }else if(questionNum==1) {
+         question.setIcon(que1);   
+      }else if(questionNum==0) {
+         question.setIcon(que0);   
+      }
+       //       
 //       // 틀린부분을 다 찾으면 메세지 출력
-//       if (f1 == true && f2 == true && f3 == true && f4 == true) {
-//        JOptionPane.showMessageDialog(null, "축하합니다.");
+       if (f1 == true && f2 == true && f3 == true && f4 == true) {
+        System.out.println("축하합니다. 다 맞추셨습니다.");
+        //여기다가 clear글자를 띄웁니다.
+
+        //clear.setVisible(true);
+        timerStop();
+        ActionListener andthenClear = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+              
+              //여기다가 다음화면으로 넘어가는 코드를 넣습니다.
+              endtimer.stop();
+            }
+          };
+          
+          endtimer = new Timer(1000, andthenClear);
+          endtimer.start();
+          endtimer.isRunning();
+       }
 //        //다 완료하면 다시 시작을 위해 동그라미 갱신
 //        f1 = false;
 //        f2 = false;
@@ -159,26 +245,30 @@ public class panel_stage1 extends JPanel implements ActionListener  {
 //        bg.repaint();
 //        bg2.repaint();
 //       }// if end
-      	}// mouseReleased()
-      }//CustomMouseAdapter{}
+         }// mouseReleased()
+   }//CustomMouseAdapter{}
 
 
    public void timerStop() {
-		timer.stop();		
-	}
-//	public void LifeControl() {
+      timer.stop();      
+   }
+   
+   public void timerStart() {
+	      timer.start();      
+	   }
+//   public void LifeControl() {
 //
-//		if (lifeNum==3) {
-//			lifeIcon = life3;
-//			System.out.println("실행됩니다.");
-//			}
-//		else if (lifeNum==2) {
-//			lifeIcon = life2;
-//			}
-//		else {
-//			lifeIcon = life1;
-//			}
-//		}
+//      if (lifeNum==3) {
+//         lifeIcon = life3;
+//         System.out.println("실행됩니다.");
+//         }
+//      else if (lifeNum==2) {
+//         lifeIcon = life2;
+//         }
+//      else {
+//         lifeIcon = life1;
+//         }
+//      }
    @Override
       public void update(Graphics g) {
          // TODO Auto-generated method stub
@@ -189,24 +279,26 @@ public class panel_stage1 extends JPanel implements ActionListener  {
    public void paint(Graphics g) {
 
       super.paint(g);
-      // 동그라미 색깔 빨강으로 표시
-      g.setColor(Color.RED);
       // 틀린그림부분에 동그라미 그리기
       if (f1 == true) {
-         g.drawImage(cir, 252, 130, 60, 60, this);      //파란애
-         g.drawImage(cir, 742, 130, 60, 60, this);
+         g.drawImage(cir, 250, 135, 60, 60, this);      //파란애
+         g.drawImage(cir, 745, 135, 60, 60, this);
       } 
       if (f2 == true) {
-         g.drawImage(cir, 7, 225, 60, 60, this);      //가방
+         g.drawImage(cir, 0, 225, 60, 60, this);      //가방
          g.drawImage(cir, 499, 225, 60, 60, this);
       }
       if (f3 == true) {
           g.drawImage(cir, 442, 145, 60, 60, this);      //구름
-          g.drawImage(cir, 932, 145, 60, 60, this);
+          g.drawImage(cir, 935, 145, 60, 60, this);
       }
       if (f4 == true) {
-          g.drawImage(cir, 123, 83, 60, 60, this);      //거북이 등
-          g.drawImage(cir, 610, 83, 60, 60, this);
+          g.drawImage(cir, 120, 87, 60, 60, this);      //거북이 등
+          g.drawImage(cir, 610, 87, 60, 60, this);
+      }
+      
+      if (f1==true && f2==true && f3==true && f4==true) {
+         g.drawImage(cle, 345, 135, 290, 260, this);
       }
    }
     
