a4d527baf034cc0fdedd18e4c45c1a8b3e6a897f
ParkJongGun <33774031+JongGunPark@users.noreply.github.com>
2017-12-18 03:18:56 +0900

    Add files via upload
    실패 화면과 하는 방법 화면에 대한 전체적인 구성과 연결을 완료하였다.

--- src/game/panel_fail.java
+++ src/game/panel_fail.java
@@ -21,27 +21,38 @@ import javax.swing.Timer;
 
 
 public class panel_fail extends JPanel implements ActionListener  {
+	public JButton btnRego;
 	frame fr;
 	public panel_fail(frame fr) {
 		this.fr=fr;
-		ImageIcon img = new ImageIcon("fail.gif","How_to");
+		setBackground(Color.BLACK);
+		ImageIcon img = new ImageIcon("fail.gif","Crying Picka");
+		ImageIcon lose = new ImageIcon("lose.jpg","You Lose");
+	    ImageIcon rego = new ImageIcon("rego3.png","regame");
+	    ImageIcon rego2 = new ImageIcon("rego4.png");
 		JLabel bg = new JLabel(img);
-		setBounds(0, 0, 600, 600);
+		JLabel tt = new JLabel(lose);
+		setBounds(0, 0,1000, 600);
 		setLayout(null);
-
+		tt.setBounds(300,0,400, 130);
 		bg.setBounds(0,0,1000, 600);
+	    btnRego = new JButton(rego);
+	    btnRego.addActionListener(this);
+	    btnRego.setBorderPainted(false);
+	    btnRego.setContentAreaFilled(false);
+	    btnRego.setRolloverIcon(rego2);
+	    btnRego.setBounds(150,100,700,800);
+		add(tt);
 		add(bg);
+		add(btnRego);
 		setVisible(true);
 		
 	}	
 
-		
-	
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void actionPerformed(ActionEvent e) {
+	         fr.change2("panel_main");		
 	}
 
 }
--- src/game/panel_howto.java
+++ src/game/panel_howto.java
@@ -9,44 +9,45 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class panel_howto extends JPanel implements ActionListener {
-	
-	frame fr;
-	public JButton butt;
-	
-	public panel_howto(frame fr) {
-		this.fr = fr;
-		
-		ImageIcon img = new ImageIcon("배경.jpg","배경");
-		
-		ImageIcon im= new ImageIcon("START.png","게임시작");
-		ImageIcon im2= new ImageIcon("START2.png");
-		
-		setBounds(0, 0, 1000, 600);
-		JLabel bg = new JLabel(img);
-		
-		butt = new JButton("게임시작");
-		butt.setIcon(im);
-		butt.addActionListener(this);
-		butt.setBorderPainted(false);
-		butt.setContentAreaFilled(false);
-		butt.setFocusPainted(false);
-		butt.setRolloverIcon(im2);
-		
-		
-		setBounds(0, 0, 1000, 600);
-		setLayout(null);
-		
-		butt.setBounds(650,350,197, 80);
-		add(butt);
-		bg.setBounds(0,0,1000, 600);
-		add(bg);
-		setVisible(true);
-		
-	}	
+   
+   frame fr;
+   public JButton butt;
+   
+   public panel_howto(frame fr) {
+      this.fr = fr;
+      
+      ImageIcon img = new ImageIcon("배경.jpg","배경");
+      
+      ImageIcon im= new ImageIcon("START.png","게임시작");
+      ImageIcon im2= new ImageIcon("START2.png");
+      
+      setBounds(0, 0, 1000, 600);
+      JLabel bg = new JLabel(img);
+      
+      butt = new JButton("게임시작");
+      butt.setIcon(im);
+      butt.addActionListener(this);
+      butt.setBorderPainted(false);
+      butt.setContentAreaFilled(false);
+      butt.setFocusPainted(false);
+      butt.setRolloverIcon(im2);
+      
+      
+      setBounds(0, 0, 1000, 600);
+      setLayout(null);
+      
+      butt.setBounds(650,350,197, 80);
+      add(butt);
+      bg.setBounds(0,0,1000, 600);
+      add(bg);
+      setVisible(true);
+      
+   }   
 
-	
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand() == "게임시작") 
-			fr.change("panel_stage1");
-	}
+
+   
+   public void actionPerformed(ActionEvent e) {
+      if (e.getActionCommand() == "게임시작") 
+         fr.change2("panel_stagep1");
+   }
 }
